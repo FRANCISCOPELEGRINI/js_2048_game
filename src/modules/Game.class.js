fb4df431df3526dc9131315199fcd92243c11d1e
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2026-07-14 18:35:50 -0300

    JS-2048-GAME 1.0

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -5,40 +5,160 @@
  * Now it has a basic structure, that is needed for testing.
  * Feel free to add more props and methods if needed.
  */
-class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+const fusaoTabuleiroDefault = [
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, false, false],
+];
+
+function genarateNumber(tabuleiro) {
+  const numero = Math.random() < 0.9 ? 2 : 4;
+  const vazios = [];
+
+  tabuleiro.forEach((linha, r) => {
+    linha.forEach((valor, o) => {
+      if (valor === 0) {
+        vazios.push([r, o]);
+      }
+    });
+  });
+
+  if (vazios.length === 0) {
+    return tabuleiro;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  const [i, j] = vazios[Math.floor(Math.random() * vazios.length)];
+
+  tabuleiro[i][j] = numero;
+
+  return tabuleiro;
+}
+class Game {
+  constructor(initialState) {
+    this.tabuleiro = initialState.map((linha) => [...linha]);
+    this.initialState = initialState.map((linha) => [...linha]);
+    this.fusaoTabuleiro = fusaoTabuleiroDefault.map((linha) => [...linha]);
+    this.soma = 0;
+  }
+
+  moveLeft() {
+    const copia = JSON.stringify(this.tabuleiro);
+
+    this.empurrarEsquerda();
+
+    for (let i = 0; i <= 3; i++) {
+      for (let o = 3; o >= 1; o--) {
+        if (
+          this.tabuleiro[i][o] === this.tabuleiro[i][o - 1] &&
+          this.fusaoTabuleiro[i][o] === false
+        ) {
+          this.tabuleiro[i][o] = 0;
+          this.tabuleiro[i][o - 1] *= 2;
+          this.soma += this.tabuleiro[i][o - 1];
+          this.fusaoTabuleiro[i][o - 1] = true;
+        }
+      }
+    }
+
+    this.empurrarEsquerda();
+    this.fusaoTabuleiro = fusaoTabuleiroDefault.map((linha) => [...linha]);
+
+    if (copia !== JSON.stringify(this.tabuleiro)) {
+      this.tabuleiro = genarateNumber(this.tabuleiro);
+    }
+  }
+  moveRight() {
+    const copia = JSON.stringify(this.tabuleiro);
+
+    this.empurrarDireita();
+
+    for (let i = 0; i <= 3; i++) {
+      for (let o = 0; o <= 2; o++) {
+        if (
+          this.tabuleiro[i][o] === this.tabuleiro[i][o + 1] &&
+          this.fusaoTabuleiro[i][o] === false
+        ) {
+          this.tabuleiro[i][o] = 0;
+          this.tabuleiro[i][o + 1] *= 2;
+          this.soma += this.tabuleiro[i][o + 1];
+          this.fusaoTabuleiro[i][o + 1] = true;
+        }
+      }
+    }
+
+    this.empurrarDireita();
+    this.fusaoTabuleiro = fusaoTabuleiroDefault.map((linha) => [...linha]);
+
+    if (copia !== JSON.stringify(this.tabuleiro)) {
+      this.tabuleiro = genarateNumber(this.tabuleiro);
+    }
+  }
+  moveUp() {
+    const copia = JSON.stringify(this.tabuleiro);
+
+    this.empurrarCima();
+
+    for (let i = 0; i <= 3; i++) {
+      for (let o = 3; o >= 1; o--) {
+        if (
+          this.tabuleiro[o][i] === this.tabuleiro[o - 1][i] &&
+          this.fusaoTabuleiro[o][i] === false
+        ) {
+          this.tabuleiro[o][i] = 0;
+          this.tabuleiro[o - 1][i] *= 2;
+          this.soma += this.tabuleiro[o - 1][i];
+          this.fusaoTabuleiro[o - 1][i] = true;
+        }
+      }
+    }
+    this.empurrarCima();
+    this.fusaoTabuleiro = fusaoTabuleiroDefault.map((linha) => [...linha]);
+
+    if (copia !== JSON.stringify(this.tabuleiro)) {
+      this.tabuleiro = genarateNumber(this.tabuleiro);
+    }
+  }
+  moveDown() {
+    const copia = JSON.stringify(this.tabuleiro);
+
+    this.empurrarBaixo();
+
+    for (let i = 0; i <= 3; i++) {
+      for (let o = 0; o <= 2; o++) {
+        if (
+          this.tabuleiro[o][i] === this.tabuleiro[o + 1][i] &&
+          this.fusaoTabuleiro[o][i] === false
+        ) {
+          this.tabuleiro[o][i] = 0;
+          this.tabuleiro[o + 1][i] *= 2;
+          this.soma += this.tabuleiro[o + 1][i];
+          this.fusaoTabuleiro[o + 1][i] = true;
+        }
+      }
+    }
+
+    this.empurrarBaixo();
+    this.fusaoTabuleiro = fusaoTabuleiroDefault.map((linha) => [...linha]);
+
+    if (copia !== JSON.stringify(this.tabuleiro)) {
+      this.tabuleiro = genarateNumber(this.tabuleiro);
+    }
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.soma;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.tabuleiro;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +170,129 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+
+  getStatus() {
+    if (JSON.stringify(this.tabuleiro) === JSON.stringify(this.initialState)) {
+      return 'idle';
+    }
+
+    let temZero = false;
+
+    for (let i = 0; i < 4; i++) {
+      for (let o = 0; o < 4; o++) {
+        const valor = this.tabuleiro[i][o];
+
+        if (valor === 0) {
+          temZero = true;
+        }
+
+        if (valor === 2048) {
+          return 'win';
+        }
+
+        if (o < 3 && valor === this.tabuleiro[i][o + 1]) {
+          return 'playing';
+        }
+
+        if (i < 3 && valor === this.tabuleiro[i + 1][o]) {
+          return 'playing';
+        }
+      }
+    }
+
+    if (temZero) {
+      return 'playing';
+    }
+
+    return 'lose';
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.tabuleiro = genarateNumber(this.tabuleiro);
+    this.tabuleiro = genarateNumber(this.tabuleiro);
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.tabuleiro = this.initialState.map((linha) => [...linha]);
+    this.soma = 0;
+    this.fusaoTabuleiro = fusaoTabuleiroDefault.map((linha) => [...linha]);
+  }
 
   // Add your own methods here
+  empurrarDireita() {
+    for (let i = 0; i <= 3; i++) {
+      for (let o = 0; o <= 2; o++) {
+        if (this.tabuleiro[i][o + 1] === 0) {
+          this.tabuleiro[i][o + 1] = this.tabuleiro[i][o];
+          this.tabuleiro[i][o] = 0;
+        }
+      }
+
+      for (let o = 0; o <= 2; o++) {
+        if (this.tabuleiro[i][o + 1] === 0) {
+          this.tabuleiro[i][o + 1] = this.tabuleiro[i][o];
+          this.tabuleiro[i][o] = 0;
+        }
+      }
+    }
+  }
+  empurrarEsquerda() {
+    for (let i = 0; i <= 3; i++) {
+      for (let o = 3; o >= 1; o--) {
+        if (this.tabuleiro[i][o - 1] === 0) {
+          this.tabuleiro[i][o - 1] = this.tabuleiro[i][o];
+          this.tabuleiro[i][o] = 0;
+        }
+      }
+
+      for (let o = 3; o >= 1; o--) {
+        if (this.tabuleiro[i][o - 1] === 0) {
+          this.tabuleiro[i][o - 1] = this.tabuleiro[i][o];
+          this.tabuleiro[i][o] = 0;
+        }
+      }
+    }
+  }
+  empurrarCima() {
+    for (let i = 0; i <= 3; i++) {
+      for (let o = 3; o >= 1; o--) {
+        if (this.tabuleiro[o - 1][i] === 0) {
+          this.tabuleiro[o - 1][i] = this.tabuleiro[o][i];
+          this.tabuleiro[o][i] = 0;
+        }
+      }
+
+      for (let o = 3; o >= 1; o--) {
+        if (this.tabuleiro[o - 1][i] === 0) {
+          this.tabuleiro[o - 1][i] = this.tabuleiro[o][i];
+          this.tabuleiro[o][i] = 0;
+        }
+      }
+    }
+  }
+  empurrarBaixo() {
+    for (let i = 0; i <= 3; i++) {
+      for (let o = 0; o <= 2; o++) {
+        if (this.tabuleiro[o + 1][i] === 0) {
+          this.tabuleiro[o + 1][i] = this.tabuleiro[o][i];
+          this.tabuleiro[o][i] = 0;
+        }
+      }
+
+      for (let o = 0; o <= 2; o++) {
+        if (this.tabuleiro[o + 1][i] === 0) {
+          this.tabuleiro[o + 1][i] = this.tabuleiro[o][i];
+          this.tabuleiro[o][i] = 0;
+        }
+      }
+    }
+  }
 }
 
-module.exports = Game;
+export default Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,120 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+import Game from '../modules/Game.class';
 
-// Write your code here
+const initialState = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+
+const game = new Game(initialState);
+
+const cells = [...document.querySelectorAll('.field-cell')];
+const scoreElement = document.querySelector('.game-score');
+const button = document.querySelector('.button');
+
+const startMessage = document.querySelector('.message-start');
+const winMessage = document.querySelector('.message-win');
+const loseMessage = document.querySelector('.message-lose');
+
+function hideMessages() {
+  startMessage.classList.add('hidden');
+  winMessage.classList.add('hidden');
+  loseMessage.classList.add('hidden');
+}
+
+function updateMessages() {
+  hideMessages();
+
+  switch (game.getStatus()) {
+    case 'idle':
+      startMessage.classList.remove('hidden');
+      break;
+
+    case 'win':
+      winMessage.classList.remove('hidden');
+      break;
+
+    case 'lose':
+      loseMessage.classList.remove('hidden');
+      break;
+  }
+}
+
+function renderBoard() {
+  const board = game.getState();
+
+  cells.forEach((cell, index) => {
+    const row = Math.floor(index / 4);
+    const col = index % 4;
+
+    const value = board[row][col];
+
+    cell.className = 'field-cell';
+
+    if (value !== 0) {
+      cell.textContent = value;
+      cell.classList.add(`field-cell--${value}`);
+    } else {
+      cell.textContent = '';
+    }
+  });
+}
+
+function updateScore() {
+  scoreElement.textContent = game.getScore();
+}
+
+function render() {
+  renderBoard();
+  updateScore();
+  updateMessages();
+}
+
+button.addEventListener('click', () => {
+  if (game.getStatus() === 'idle') {
+    game.start();
+
+    button.textContent = 'Restart';
+    button.classList.remove('start');
+    button.classList.add('restart');
+  } else {
+    game.restart();
+    game.start();
+  }
+
+  render();
+});
+
+document.addEventListener('keydown', (evento) => {
+  if (game.getStatus() === 'idle') {
+    return;
+  }
+
+  switch (evento.key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+
+    default:
+      return;
+  }
+
+  render();
+});
+
+render();
